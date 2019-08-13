ca3fd3b05ec73358e15988d07cdd2aa9ec3848de
suhaoran <suhaoran@imdada.cn>
2019-08-13 17:48:28 +0800

    fixbubg

--- src/main/java/com/github/binarywang/demo/wx/mp/handler/MsgHandler.java
+++ src/main/java/com/github/binarywang/demo/wx/mp/handler/MsgHandler.java
@@ -1,20 +1,13 @@
 package com.github.binarywang.demo.wx.mp.handler;
 
 import com.github.binarywang.demo.wx.mp.builder.TextBuilder;
-import com.github.binarywang.demo.wx.mp.utils.JsonUtils;
-import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.api.impl.WxMpServiceHttpClientImpl;
-import me.chanjar.weixin.mp.bean.WxMpSemanticQuery;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
-import me.chanjar.weixin.mp.bean.result.WxMpSemanticQueryResult;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 import static me.chanjar.weixin.common.api.WxConsts.XmlMsgType;
@@ -52,27 +45,27 @@ public class MsgHandler extends AbstractHandler {
 
         String str = "发单";
         String address = wxMessage.getContent();
-        String content = "";
-        if (StringUtils.startsWith(address, "发单")) {
-            address = address.replaceFirst("发单", "");
-            WxMpSemanticQuery wxMpSemanticQuery = new WxMpSemanticQuery();
-            wxMpSemanticQuery.setQuery(address);
-            wxMpSemanticQuery.setCategory("LOC_POI");
-            wxMpSemanticQuery.setLatitude(121.76F);
-            wxMpSemanticQuery.setLongitude(31.05F);
-            wxMpSemanticQuery.setCity("上海");
-            wxMpSemanticQuery.setAppid("wxafcb7752886aa01b");
-            wxMpSemanticQuery.setUid(wxMessage.getFromUser());
-
-            WxMpSemanticQueryResult result = null;
-            try {
-                result = wx.semanticQuery(wxMpSemanticQuery);
-            } catch (WxErrorException e) {
-                e.printStackTrace();
-            }
-            //TODO 组装回复消息
-             content = "收到信息内容：" + JsonUtils.toJson(result);
-        }
+        String content = "默认返回值";
+//        if (StringUtils.startsWith(address, "发单")) {
+//            address = address.replaceFirst("发单", "");
+//            WxMpSemanticQuery wxMpSemanticQuery = new WxMpSemanticQuery();
+//            wxMpSemanticQuery.setQuery(address);
+//            wxMpSemanticQuery.setCategory("LOC_POI");
+//            wxMpSemanticQuery.setLatitude(121.76F);
+//            wxMpSemanticQuery.setLongitude(31.05F);
+//            wxMpSemanticQuery.setCity("上海");
+//            wxMpSemanticQuery.setAppid("wxafcb7752886aa01b");
+//            wxMpSemanticQuery.setUid(wxMessage.getFromUser());
+//
+//            WxMpSemanticQueryResult result = null;
+//            try {
+//                result = wx.semanticQuery(wxMpSemanticQuery);
+//            } catch (WxErrorException e) {
+//                e.printStackTrace();
+//            }
+//            //TODO 组装回复消息
+//             content = "收到信息内容：" + JsonUtils.toJson(result);
+//        }
 
         return new TextBuilder().build(content, wxMessage, weixinService);
 
