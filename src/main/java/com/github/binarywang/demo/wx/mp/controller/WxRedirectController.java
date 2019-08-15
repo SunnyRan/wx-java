8f736350d010e730d6a92de57710fbd86e13c87c
suhaoran <suhaoran@imdada.cn>
2019-08-15 15:40:30 +0800

    增加存储用户信息列表

--- src/main/java/com/github/binarywang/demo/wx/mp/controller/WxRedirectController.java
+++ src/main/java/com/github/binarywang/demo/wx/mp/controller/WxRedirectController.java
@@ -1,19 +1,19 @@
 package com.github.binarywang.demo.wx.mp.controller;
 
-import com.github.binarywang.demo.wx.mp.config.WxMpConfiguration;
+import com.github.binarywang.demo.wx.mp.cache.CacheManager;
 import lombok.AllArgsConstructor;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
 import me.chanjar.weixin.mp.bean.result.WxMpUser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.xml.ws.Action;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Edward
@@ -33,7 +33,17 @@ public class WxRedirectController {
         try {
             WxMpOAuth2AccessToken accessToken = wxService.oauth2getAccessToken(code);
             WxMpUser user = wxService.oauth2getUserInfo(accessToken, null);
-            map.put("user", user);
+
+            //存储用户信息
+            Map userMap = CacheManager.get("userList", HashMap.class);
+            if(userMap == null){
+                userMap = new HashMap();
+            }
+            userMap.put(user.getOpenId(),user);
+            CacheManager.put("userList",userMap);
+
+
+            map.put("user", userMap);
         } catch (WxErrorException e) {
             e.printStackTrace();
         }
